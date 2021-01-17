c3fa185470506fd0cc7a099a971d3d5c0993f0f9
ssalman172 <54460907+ssalman172@users.noreply.github.com>
2021-01-17 20:52:28 +0700

    Update PERPUS.sql

--- PERPUS.sql
+++ PERPUS.sql
@@ -1,5 +1,5 @@
 CREATE TABLE peminjam (
-    peminjam_id             NUMBER(9) NOT NULL,
+    peminjam_id             NUMBER(9) PRIMARY KEY NOT NULL,
     nama_lengkap            VARCHAR2(100) NOT NULL,
     gender                  VARCHAR2(2) NOT NULL,
     id_pinjam               NUMBER(10) NOT NULL,
@@ -7,13 +7,13 @@ CREATE TABLE peminjam (
 );
 
 CREATE TABLE peminjaman (
-    pinjam_id                  NUMBER(10) NOT NULL,
+    pinjam_id                  NUMBER(10) PRIMARY KEY NOT NULL,
     tanggal_peminjaman         DATE NOT NULL,
     tanggal_pengembalian       DATE NOT NULL
 );
 
 CREATE TABLE buku (
-    buku_id               NUMBER(10) NOT NULL,
+    buku_id               NUMBER(10) PRIMARY KEY NOT NULL,
     nama_buku             VARCHAR2(100 CHAR) NOT NULL,
     jumlah_halaman        NUMBER(5),
     tipe_buku             VARCHAR2(20),
@@ -27,18 +27,18 @@ CREATE TABLE ganti_buku (
 );
 
 CREATE TABLE penerbit (
-    penerbit_id    NUMBER(7) NOT NULL,
+    penerbit_id    NUMBER(7) PRIMARY KEY NOT NULL,
     nama_penerbit  VARCHAR2(30) NOT NULL,
     alamat         VARCHAR2(60)
 );
 
 CREATE TABLE sanksi (
-    sanksi_id           NUMBER(7) NOT NULL,
+    sanksi_id           NUMBER(7) PRIMARY KEY NOT NULL,
     jenis_pelanggaran   VARCHAR2(30) NOT NULL
 );
 
 CREATE TABLE denda (
-    denda_id        NUMBER(5) NOT NULL,
+    denda_id        NUMBER(5) PRIMARY KEY NOT NULL,
     denda_perhari   INTEGER NOT NULL,
     total_denda     INTEGER NOT NULL
 );
@@ -48,9 +48,6 @@ CREATE TABLE status_anggota (
     status_sanksi   NUMBER(1) NOT NULL
 );
 
-ALTER TABLE peminjam 
-ADD CONSTRAINT peminjam_pk PRIMARY KEY ( peminjam_id );
-
 ALTER TABLE peminjam
 ADD CONSTRAINT peminjam_pinjaman_fk FOREIGN KEY ( id_pinjam ) 
 REFERENCES pinjaman ( pinjam_id );
@@ -59,21 +56,6 @@ ALTER TABLE peminjam
 ADD CONSTRAINT peminjam_status_anggota_fk FOREIGN KEY ( status_sanksi_peminjam )
 REFERENCES status_anggota ( status_sanksi );
 
-ALTER TABLE peminjaman 
-ADD CONSTRAINT peminjaman_pk PRIMARY KEY ( pinjam_id );
-
-ALTER TABLE buku 
-ADD CONSTRAINT buku_pk PRIMARY KEY ( buku_id );
-
 ALTER TABLE buku
 ADD CONSTRAINT buku_penerbit_fk FOREIGN KEY ( id_penerbit)
 REFERENCES penerbit ( penerbit_id );
-
-ALTER TABLE penerbit
-ADD CONSTRAINT penerbit_pk PRIMARY KEY ( penerbit_id );
-
-ALTER TABLE sanksi 
-ADD CONSTRAINT sanksi_pk PRIMARY KEY ( sanksi_id );
-
-ALTER TABLE denda
-ADD CONSTRAINT denda_pk PRIMARY KEY ( denda_id );
